5713b969d05e24760e35807022a1f187b27cb2f1
Abdel Kader Diarra <abdelkaderdiarra707@gmail.com>
2025-08-17 15:49:59 +0000

    Regler le compte de rendu

--- server.js
+++ server.js
@@ -196,6 +196,6 @@ app.get('/api/benefices', async (req, res) => {
 
 /* --- DÉMARRAGE DU SERVEUR --- */
 app.listen(PORT, () => {
-  console.log('✅ Connexion à la base de données réussie Daff');
+  console.log('✅ Connexion à la base de données réussie');
   console.log(`🚀 Serveur backend lancé sur le port ${PORT}`);
 });
